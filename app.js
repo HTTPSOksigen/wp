7c89fbab3ae07c852498b6bce203c27fbd78d1fc
HTTPSOksigen <142164437+HTTPSOksigen@users.noreply.github.com>
2023-08-13 19:18:05 +0400

    Update app.js
    asdasd

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { Client } = require('whatsapp-web.js')
 const app = express();
 const server = http.createServer(app)
 const io = new Server(server)
-
+//asdasd
 const port = 3000;
 
 app.use(express.static('public'))
